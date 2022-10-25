f5dc9a72470c40880d5c9042c11fe9b895f3f1d2
Andrew Heim <andrewsheim@gmail.com>
2022-10-24 17:27:03 -0700

    recursion review, finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,11 +8,29 @@
 //input: string of className
 //output: an array of nodes or elements that have the classname in it
 //edge cases: none
-//cosntraints: have to use recursion, or actual function
+//cosntraints: have to use recursion, or actual function, use document.body, element.childNodes, and element.classList
 
 //strategy
-//
+// Access the body of the element and iterate through entire node list using element.childNodes. Use element.classList to check if class name is equal to the input className.
 
-var getElementsByClassName = function(className) {
-
+var getElementsByClassName = function(className, node) {
+  // console.log className
+  // Declare result array
+  var result = [];
+  var node = node || document.body;
+  if (node.classList) {
+    for (var j = 0; j < node.classList.length; j++) {
+      if (node.classList[j] === className) {
+        result.push(node);
+      }
+    }
+  }
+  for (var i = 0; i < node.childNodes.length; i++) {
+    var currentChild = node.childNodes[i];
+    result = result.concat(getElementsByClassName(className, currentChild));
+  }
+  console.log(result);
+  return result;
 };
+
+
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,33 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function(json) {
-  // your code goes here
-};
+
+// I - JSON string
+// O - Value or object described by the string
+// C - Use recursion
+// E - Optional reviver function
+
+// Strategy
+//iterate through string
+  // Check for opening curly brace or bracket (array or object)
+    //for looop iterate through the string after the opening curly brace
+      //
+      //check each element if it equals a closing bracket
+
+    //create variable, array or object
+
+    //tempPosition to comma
+      //iterate through string after the open bracket
+
+    //Split the JSON string on the commas
+    // Declare storage variable
+
+
+  //check for parentheses for string
+    //return as string
+  //chck for numbers
+  //check for boolean
+
+  var parseJSON = function(json) {
+    // your code goes here
+  };
